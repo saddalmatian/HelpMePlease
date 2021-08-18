7f7964c87fd43f7ae9dccbd4a6c0e61f4610ee09
saddalmatian <saddalmatian@gmail.com>
2021-08-18 15:45:14 +0700

    Fix Docker File

--- ScrapingThings/BeautifulSoup4/bs4Setup.py
+++ ScrapingThings/BeautifulSoup4/bs4Setup.py
@@ -1,8 +1,8 @@
 import os
 os.system("docker build -t bs4_scraping .")
-os.system("docker volume create my_volume")
-os.system("docker run -it -v my_volume:/getData --name bs4_container bs4_scraping")
+os.system("docker volume create my_volume1")
+os.system("docker run -it -v my_volume1:/getData --name bs4_container bs4_scraping")
 os.system("docker cp bs4_container:/getData/data.json .")
 os.system("docker rm bs4_container")  
-os.system("docker volume rm my_volume")
+os.system("docker volume rm my_volume1")
 
--- ScrapingThings/Xpath/lxmlSetup.py
+++ ScrapingThings/Xpath/lxmlSetup.py
@@ -1,8 +1,8 @@
 import os
 os.system("docker build -t lxml_scraping .")
-os.system("docker volume create my_volume")
-os.system("docker run -it -v my_volume:/getData --name lxml_container lxml_scraping")
+os.system("docker volume create my_volume2")
+os.system("docker run -it -v my_volume2:/getData --name lxml_container lxml_scraping")
 os.system("docker cp lxml_container:/getData/data.json .")
 os.system("docker rm lxml_container")  
-os.system("docker volume rm my_volume")
+os.system("docker volume rm my_volume2")
 
